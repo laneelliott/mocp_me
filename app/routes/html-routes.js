621992dc7156cea4c5b23057b28885869dd38b25
izaweslie <izaweslie@gmail.com>
2017-12-11 21:10:40 -0600

    adding up to date html routes

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -1,33 +1,33 @@
-// *********************************************************************************
 // html-routes.js - this file offers a set of routes for sending users to the various html pages
-// *********************************************************************************
 
-// Dependencies
-// =============================================================
+//Dependencies
+
 var path = require("path");
 
+//Routes
 
-// Routes
-// =============================================================
 module.exports = function(app) {
 
-  // Each of the below routes just handles the HTML page that the user gets sent to.
-
-  // index route loads view.html
+  //Each of the below routes handles the HTML page that the user gets sent to.
+  
+  // index route loads main page
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/view.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  // add route loads the add.html page,
-  // where users can enter new characters to the db
-  app.get("/add", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/add.html"));
-  });
+  // saving this code in case we want to add additional pages
+  // app.get("/uploadimg", function(req, res) {
+  // res.sendFile(path.join(__dirname, "../public/add.html"));
+  // });
 
-  // all route loads the all.html page,
-  // where all characters in the db are displayed
-  app.get("/all", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/all.html"));
-  });
+  // // blog route loads page with match image from MoCP
+  // app.get("/itsamatch", function(req, res) {
+  // res.sendFile(path.join(__dirname, "../public/all.html"));
+  // });
+
+  // // authors route loads user crowdsourced tagging
+  // app.get("/giveitatag", function(req, res) {
+  // res.sendFile(path.join(__dirname, "../public/view.html"));
+  // });
 
 };
