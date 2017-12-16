4daa2af90e7ac07a4fe1fea2fac8189e1957a372
izaweslie <izaweslie@gmail.com>
2017-12-16 14:06:50 -0600

    api-route testing findOne in mocp_db photos table

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,64 +1,21 @@
-// *********************************************************************************
-// api-routes.js - this file offers a set of routes for displaying and saving data to the db
-// *********************************************************************************
+var db = require("../models");
 
-// Dependencies
-// =============================================================
-var Photos = require("../models/photos.js");
-var Tags = require("../models/mocptags.js")
 
 // Routes
-// =============================================================
 module.exports = function(app) {
 
   // Search for Specific Photos (or all Photos) then provides JSON
-  app.get("/api/:Photos?", function(req, res) {
+  app.get("/api", function(req, res) {
 
-    // If the user provides a specific Photos in the URL...
-    if (req.params.Photos) {
-
-      // Then display the JSON for ONLY that Photo.
-      // (Note how we're using the ORM here to run our searches)
       Photos.findOne({
         where: {
-          routeName: req.params.Photos
+          id: 234
         }
         // WHERE routename = ?, [req.params.Photos]
       }).then(function(result) {
         console.log(result);
         return res.json(result);
       });
-    }
-
-    // Otherwise...
-    else {
-      // Otherwise display the data for all of the Photos.
-      // (Note how we're using Sequelize here to run our searches)
-      Photos.findAll({})
-        .then(function(result) {
-          return res.json(result);
-        });
-    }
-
   });
 
-  // If a user sends data to add a new Photo...
-  app.post("/api/new", function(req, res) {
-
-    // Take the request...
-    var Photos = req.body;
-
-    // Create a routeName
-    var routeName = Photos.name.replace(/\s+/g, "").toLowerCase();
-
-    // Then add the Photos to the database using sequelize
-    Photos.create({
-      routeName: routeName,
-      name: Photos.name,
-      path: Photos.path,
-      web_path: Photos.web_path,
-      artist_id: Photos.artist_id
-    });
-
-  });
 };
