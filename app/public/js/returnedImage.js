9012467d1c52cc540eacb664e5ba977b68ead0ec
Amanda Yamasaki <amandayamasaki@u.northwestern.edu>
2017-12-17 21:43:18 -0600

    updates

--- app/public/js/returnedImage.js
+++ app/public/js/returnedImage.js
@@ -2,8 +2,9 @@
 var returnedImageURL = null; 
 
 function getURL() {
-    $.get("/...", function(data) {
-      returnedImageURL = data;
+    $.get("/api/tags/test-one", function(data) {
+      var returnedImageURL = JSON.stringify(data[0].web_path);
+      console.log(returnedImageURL);
       $("#mocp-image-one").attr('src', returnedImageURL);
     });
 }
