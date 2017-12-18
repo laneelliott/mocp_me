10d28f1a9e0e7264c7f21beaa0c2b6f7d177b1bd
hnwangwa <hollynwangwa@gmail.com>
2017-12-17 23:35:01 -0600

    added ajax call
    incorporating an ajax call in lieu of sending base 64 to backend (mayday)

--- app/public/js/imageupload.js
+++ app/public/js/imageupload.js
@@ -5,22 +5,73 @@
   var reader = new FileReader()
   reader.onloadend = processFile
   reader.readAsDataURL(file);
-  // console.log(reader); 
+  console.log(reader); 
 }
 
 //Encodes the new base 64img
 function processFile(event) {
   var encodedFile = event.target.result;
-  console.log(encodedFile);
-
+  // console.log(encodedFile);
+  sendFiletoCloudVision(encodedFile);
 
 }
+function sendFiletoCloudVision(file){
+  var type = 'LABEL_DETECTION';
+  //This will currently only allow jpeg images
+  var fileType = file.split(',');
+  fileType = fileType[0] + ",";
+  console.log(fileType)
+  var content = file.replace(fileType, "");
+  showImage(content)
+    // Strip out the file prefix when you convert to json.
+    var json = {
+     "requests": [
+     { 
+       "image": {
+         "content": content 
+       },
+       "features": [
+       {
+         "type": type,
+         "maxResults": 10
+       }
+       ]
+     }
+     ]
+   }
+    //console.log(JSON.stringify(json));
+    json = JSON.stringify(json)
+//console.log(JSON.stringify(json));
+json = JSON.stringify(json)
 
+  //Vision AJAX Request
+  $.ajax({
+    type: 'POST',
+    url: "https://vision.googleapis.com/v1/images:annotate?key=" + api_key,
+    dataType: 'json',
+    data: json,
+      //Include headers, otherwise you get an odd 400 error.
+      headers: {
+        "Content-Type": "application/json",
+      },
+      success: function(data, textStatus, jqXHR) {
+        displayJSON(data);
+        //console.log(data);
+        //console.log(textStatus)
+        //console.log(jqXHR)
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log(jqXHR);
+        console.log(textStatus);
+        console.log(errorThrown);
+      }
+    });
+}
 // // //Encodes the new base 64img
-// function postBASE64(encodedFile){
-//     //Post information to route.
-//     $.post("/api/new", encodedFile)
-//       .then();
-// }
+function postBASE64(encodedFile){
+    //Post information to route.
+    $.post("/api/new", encodedFile)
+      .then();
+}
 
 
